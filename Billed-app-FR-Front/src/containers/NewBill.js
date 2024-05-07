380580ba509ad45dcf9d6ac87b1d6b0335748f3a
pierregaly33 <galy.pierre31@gmail.com>
2024-05-07 11:29:37 +0200

    ajout "si  le format de l'image est incorrect  le nom de l'image n'apparait pas

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -18,6 +18,7 @@ export default class NewBill {
     handleChangeFile = (e) => {
         e.preventDefault();
         const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+        const fileInput = this.document.querySelector(`input[data-testid="file"]`);
         const filePath = e.target.value.split(/\\/g);
         const fileName = filePath[filePath.length - 1];
         const formData = new FormData();
@@ -42,6 +43,7 @@ export default class NewBill {
                 })
                 .catch((error) => console.error(error));
         } else {
+            fileInput.value = "";
             alert("Veuillez choisir un type d'image valide");
         }
     };
